88b964c364de320594dc94f0c23c710ab5dbea2d
gunasekhar20 <gunasekhar.sangaraju@gmail.com>
2024-10-23 21:00:36 +0530

    Templates

--- flaskbog.py
+++ flaskbog.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+@app.route("/about")
 def hello_world():
-    return "<p>Welcome to Flask</p>"
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
