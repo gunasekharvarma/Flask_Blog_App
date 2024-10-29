72541884df49741830bf175e5b14de475d67ce96
gunasekhar20 <gunasekhar.sangaraju@gmail.com>
2024-10-29 07:31:56 +0530

    Forms completed

--- flaskbog.py
+++ flaskbog.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from forms import RegistrationForm, LoginForm
+from flask import flash
+from flask import redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ccf2109b44758b8ea579635b1823e915'
 
 posts = [
 
@@ -26,8 +30,28 @@ def home_page():
     return render_template('home.html',posts=posts)
 
 @app.route("/about")
-def hello_world():
+def about_page():
     return render_template('about.html',title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!",'success')
+        return redirect(url_for('home_page'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "guna@gmail.com" and form.password.data == "password":
+            flash('You have sucessfully logged In', 'success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Login is Unsuccessful, Please check username and password', 'danger')
+    return render_template('login.html',title='Login',form=form)
+
 
 
 if __name__ == "__main__":
